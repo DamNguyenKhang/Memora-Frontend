3d97b8c4946edbfc6cfb947c95e2f30102e2e74e
DamNguyenKhang <nguyeenkhang15112004@gmail.com>
2025-12-22 16:44:36 +0700

    Initialize project

---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import { useState, forwardRef } from 'react';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+
+function Image({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) {
+    const [fallback, setFallback] = useState('');
+    console.log('fallback:', fallback);
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+    return (
+        <img
+            ref={ref}
+            className={classNames(styles.wrapper, className)}
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            onError={handleError}
+        />
+    );
+}
+
+export default forwardRef(Image);
---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import Home from '~/components/pages/Home';
+import config from '~/config';
+
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
